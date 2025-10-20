a30c344f50aeec01e7c6726da0078797d383a485
Antonin Grenier <drixion05@gmail.com>
2025-10-20 14:33:26 +0200

    fixed display

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,38 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "./App.css";
 
 function App() {
-    const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        async function fetchProducts() {
-            const response = await fetch("https://fakestoreapi.com/products");
-            const data = await response.json();
-            setProducts(data);
-        }
-        fetchProducts();
-    }, []);
-    
-    return (
-        <Container>
-          <Row>
-                {products.map((product) => (
-                    <Col key={product.id} md={3}  className="d-grid gap-2 col-6 mb-3">
-                    <Card >
-                        <Card.Img variant="top" src={product.image} />
-                        <Card.Body>
-                            <Card.Title>{product.title}</Card.Title>
-                            <Card.Text>{product.description}</Card.Text>
-                            <Card.Text>${product.price}</Card.Text>
-                        </Card.Body>
-                    </Card>
-                    </Col>
-                ))}
-                </Row>
-        </Container>
-    );
+	useEffect(() => {
+		async function fetchProducts() {
+			const response = await fetch("https://fakestoreapi.com/products");
+			const data = await response.json();
+			setProducts(data);
+		}
+		fetchProducts();
+	}, []);
+
+	return (
+		<Container>
+			<Row>
+				{products.map((product) => (
+					<Col key={product.id} md={3} className="gy-3">
+						<Card className="h-100">
+							<Card.Img variant="top" src={product.image} />
+							<Card.Body>
+								<Card.Title>{product.title}</Card.Title>
+								<Card.Text>{product.description}</Card.Text>
+								<Card.Text>{product.price} €</Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+				))}
+			</Row>
+		</Container>
+	);
 }
 
 export default App;
