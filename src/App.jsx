0834c1fdaef15a1f70d7b2a717a76f6f1ff78461
Antonin Grenier <drixion05@gmail.com>
2025-10-20 14:03:26 +0200

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
     const [products, setProducts] = useState(null);
@@ -16,8 +18,8 @@ function App() {
     }, []);
     
     return (
-        <Container>
-          <Row className="justify-content-md-center">
+        <Container className="justify-content-md-center">
+          <Row >
                 {products.map((product) => (
                     <Col>
                     <Card key={product.id}>
